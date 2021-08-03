696303914ac9d7d8ddaf9b26ba5e0feecbb7634e
sabrinaun <88392906+sabrinaun@users.noreply.github.com>
2021-08-03 11:45:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 #The purpose of this program is to scrape legal documents for commonly used terms and help users understand them.
 #This is the API that will help scrape web-based leasing documents for word choice
+#API is sourced from https://docs.scrapy.org/en/latest/intro/tutorial.html
 import scrapy as scrapy
 
 import os
