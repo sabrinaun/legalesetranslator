1e0934df42ca27aedd4826fccbed12d69f9c5408
sabrinaun <88392906+sabrinaun@users.noreply.github.com>
2021-08-04 13:33:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
-
 #The purpose of this program is to scrape legal documents for commonly used terms and help users understand them.
+#Inteded uses involve terms and conditions, leasing agreements, and other types of formal contracts
 #This is the API that will help scrape web-based leasing documents for word choice
 #API is sourced from https://docs.scrapy.org/en/latest/intro/tutorial.html
-import scrapy as scrapy
-
-import os
-
-os.chdir('scrubd')
-
-scrapy startproject tutorial [project_dir]
-
-cd project_dir
-
-import scrapy
-
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
